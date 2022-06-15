f0e597abcd5cdc1b65743bfa80db5199d38b4ce3
Noeme Silva <93559668+Noemesilva@users.noreply.github.com>
2022-06-15 16:14:05 -0300

    Exibindo dados para edição

--- controller/atualizaCliente-controller.js
+++ controller/atualizaCliente-controller.js
@@ -2,16 +2,27 @@ import { clienteService } from "../service/cliente-service.js"
 
 const pegaURL = new URL(window.location)
 
-const id =  pegaURL.searchParams.get('id')
+const id =  pegaURL.searchParams.get('id')//pega o id na url
 
-const inputNome = document.querySelector('[data-nome]')
+const inputNome = document.querySelector('[data-nome]')// percorre o DOM, buscando data-nome 
 const inputEmail = document.querySelector('[data-email]')
+
 
 
 clienteService.detalhaCliente(id)
 .then( dados=>{
     inputNome.value = dados.nome
     inputEmail.value = dados.email
+})
 
+const formulario = document.querySelector('[data-form]')
+
+formulario.addEventListener('submit', (evento)=> {
+    evento.preventDefault()
+
+    clienteService.atualizaCliente(id, inputNome.value, inputEmail.value)
+    .then(()=> {
+        window.location.href = "../telas/edicao_concluida.html"
+    })
 })
 
